90edd87e8f9e71eb0803505a036c91f5d069b158
Ugwuanyi Annastesia Amarachi <annajoan201@gmail.com>
2026-06-11 14:21:26 +0100

    Add overflow-x-hidden to html as well, clamp footer text on mobile

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth overflow-x-hidden">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
